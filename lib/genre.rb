2af71e9227e937f06e7a70b72e6f30a18c8b5453
Corban <sonnemanc@gmail.com>
2021-09-23 12:19:24 -0600

    commit tree 3b3ac6b4f52a7447ce62dd2197334b866e53953c

---
+++ lib/genre.rb
@@ -0,0 +1,50 @@
+require 'pry'
+
+class Genre
+  attr_accessor :name, :musiclibrarycontroller, :musicimporter
+  extend Concerns::Findable
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(genre)
+    genre = self.new(genre)
+    genre.save
+    genre
+  end
+
+  def songs
+    Song.all.select do |song|
+        song.genre == self
+    end
+  end
+
+  def artists
+    @new_array = []
+    songs.each do |song|
+      if @new_array.include?(song.artist)
+        nil
+      else
+        @new_array << song.artist
+      end
+    end
+    @new_array
+  end
+
+
+end
